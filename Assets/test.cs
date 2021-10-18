21f369c5fdb60be82c8bc2e503fdeda2ebadd6e4
Jason Witherell <jwitherell@shawnee.edu>
2021-10-18 16:09:54 -0400

    Update test.cs

--- Assets/test.cs
+++ Assets/test.cs
@@ -13,6 +13,6 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.Rotate(0, 0, 90 * Time.deltaTime);
     }
 }
